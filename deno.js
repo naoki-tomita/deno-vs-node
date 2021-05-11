99d4638c4a477d4102c0df765c26f73d3e4cb2c1
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2021-05-11 20:11:08 +0900

    Improve deno implementation

--- deno.js
+++ deno.js
@@ -1,21 +1,8 @@
-import { serve } from "https://deno.land/std@0.95.0/http/server.ts";
-
-const requests = serve({ port: 8000 });
 const decoder = new TextDecoder();
-async function parseBody(body, size) {
-  const tmpBody = new Uint8Array(size);
-  await body.read(tmpBody);
-  return JSON.parse(decoder.decode(tmpBody) || "{}")
-}
-
-async function respond(request) {
-  const body = await parseBody(request.body, request.contentLength ?? 0);
-  request.respond({
-    status: 200,
-    body: JSON.stringify(body),
-  });
-}
-
-for await (const request of requests) {
-  respond(request);
+for await (const conn of Deno.listen({ port: 8000 })) {
+  (async () => {
+    for await (const { request, respondWith } of Deno.serveHttp(conn)) {
+      respondWith(new Response(JSON.stringify(await request.json()), { status: 200 }));
+    }
+  })()
 }
